1b690aa5650c04ff5f150755ecb5743b94601137
Decode777 <vaishnavis979@gmail.com>
2023-12-20 16:00:06 +0530

    .

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+
+//components
+import Login from './components/account/Login';
+
+function App() {
+  return (
+    <div className="App">
+     <Login />
+      
+    </div>
+  );
+}
+
+export default App;

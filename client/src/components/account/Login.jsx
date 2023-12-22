88db252eaed8cf1d49c3743f92c63ab9b8ec89c2
Decode777 <vaishnavis979@gmail.com>
2023-12-23 02:13:12 +0530

    .

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,10 +1,91 @@
 import React from 'react'
+import { Box, Button, TextField, Typography, styled } from '@mui/material';
 
-function Login() {
+const Component =styled(Box)`
+    width:400px;
+    margin:auto;
+    box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`
+
+const Image = styled('img')({
+  width: 100,
+  display: 'flex',
+  margin: 'auto',
+  padding: '50px 0 0'
+});
+
+   const Wrapper = styled(Box)`
+   padding: 25px 35px;
+   display: flex;
+   flex: 1;
+   overflow: auto;
+   flex-direction: column;
+   & > div, & > button, & > p {
+       margin-top: 20px;
+   }
+   `;   
+
+   const LoginButton = styled(Button)`
+   text-transform: none;
+   background: #FB641B;
+   color: #fff;
+   height: 48px;
+   border-radius: 2px;
+`;
+
+const SignupButton = styled(Button)`
+   text-transform: none;
+   background: #fff;
+   color: #2874f0;
+   height: 48px;
+   border-radius: 2px;
+   box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
+
+const Text = styled(Typography)`
+   color: #878787;
+   font-size: 16px;
+`;
+
+const Error = styled(Typography)`
+    font-size: 10px;
+    color: #ff6161;
+    line-height: 0;
+    margin-top: 10px;
+    font-weight: 600;
+`
+
+const loginInitialValues = {
+    username: '',
+    password: ''
+};
+
+const signupInitialValues = {
+    name: '',
+    username: '',
+    password: '',
+};
+
+
+
+const Login = () => {
+
+   const imageURL='https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+
+
   return (
-    <div>
-      uhku
-    </div>
+    <Component>
+      <Box>
+      <Image src={imageURL} alt="blog" />
+      <Wrapper>
+      <TextField variant='standard' label="Enter username" />
+      <TextField variant='standard' label="Enter password" />
+      <LoginButton variant='contained'>Login</LoginButton>
+      <Text style={{ textAlign:'center'}}>OR</Text>
+      <SignupButton>Create an Account</SignupButton>
+      </Wrapper>
+      </Box>
+    </Component>  
   )
 }
 

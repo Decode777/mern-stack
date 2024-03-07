ebecb6da0eae9b197d3bb41015aab2d6c1f825e2
Decode777 <vaishnavis979@gmail.com>
2024-03-07 17:12:55 +0530

    db connect
    string pass new, db connected

--- server/database/db.js
+++ server/database/db.js
@@ -1,18 +1,11 @@
 import mongoose from "mongoose"
 
-
- const Connection =  async(username, password) => {
-    const URL =`mongodb+srv://${username}:${password}@blog-app.6dnxpr5.mongodb.net/?retryWrites=true&w=majority`;
+const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect(URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          
-        });
-        console.log('Database connected successfully');
-    }catch(error){
-        console.log('Error while connecting with the database' , error);
+        const conn= await mongoose.connect(process.env.MONGO_URL)
+        console.log(`DB is connected ${conn.connection.host}`)
+    } catch (error) {
+        console.log(`Error in MongoDB ${error}`);
     }
-};
-
-export default Connection;
+}
+export default connectDB;
--- server/index.js
+++ server/index.js
@@ -1,20 +1,12 @@
-import express from 'express';
 import dotenv from 'dotenv';
-
-
-
-import Connection from './database/db.js';
+import express from 'express';
+import connectDB from '../server/database/db.js';
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
 const PORT = 8000;
 
-
-const username= process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
-
-Connection(username, password);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
